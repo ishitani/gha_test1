ae8facddaf6eecc06076a146bae6f0d1d8503d7f
ishitani <ishitani@users.sourceforge.net>
2024-05-09 19:28:45 +0900

    add install uxp script

---
+++ install_uxp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WSDIR=$(cd $(dirname $0)/..; pwd)
+
+if [ ! -d ${WSDIR}/platform ]; then
+# Retrieve UXP tarball
+UXP_TGZ=RB_20231106.tar.gz
+UXP_VERSION=v0.0.1
+wget --progress=dot:giga -c \
+     https://github.com/CueMol/uxp_release/releases/download/$UXP_VERSION/$UXP_TGZ
+rm -rf uxp
+set +e
+tar xzf $UXP_TGZ
+set -e
+mv uxp ${WSDIR}/platform
+fi
